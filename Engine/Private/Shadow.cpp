77fb2d2accb79b954f90dad4eb4a85ec4b3d374e
Bamtoliya <31297221+Bamtoliya@users.noreply.github.com>
2025-07-07 19:41:46 +0900

    9개월 11일차 그림자기반

---
+++ Engine/Private/Shadow.cpp
@@ -0,0 +1,29 @@
+﻿#include "Shadow.h"
+
+CShadow::CShadow(ID3D11Device* pDevice, ID3D11DeviceContext* pContext)
+	: m_pDevice { pDevice }
+	, m_pContext { pContext }
+{
+	Safe_AddRef(m_pDevice);
+	Safe_AddRef(m_pContext);
+}
+
+HRESULT CShadow::Ready_Light_For_Shadow(const SHADOW_DESC& Desc)
+{
+	
+
+	return S_OK;
+}
+
+CShadow* CShadow::Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext)
+{
+	return new CShadow(pDevice, pContext);
+}
+
+void CShadow::Free()
+{
+	__super::Free();
+
+	Safe_Release(m_pDevice);
+	Safe_Release(m_pContext);
+}
---
+++ Engine/Public/Shadow.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+
+#include "Base.h"
+
+NS_BEGIN(Engine)
+
+class CShadow final : public CBase
+{
+public:
+	typedef struct tagShadowDesc
+	{
+		_float3			vEye, vAt;
+		_float			fFovy, fNear, fFar;
+	}SHADOW_DESC;
+private:
+	CShadow(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	virtual ~CShadow() = default;
+
+public:
+	HRESULT Ready_Light_For_Shadow(const SHADOW_DESC& Desc);
+
+private:
+	ID3D11Device*					m_pDevice = { nullptr };
+	ID3D11DeviceContext*			m_pContext = { nullptr };
+
+private:
+	_float4x4						m_LightViewMatrix = {};
+	_float4x4						m_LightProjMatrix = {};
+
+public:
+	static CShadow* Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	virtual void Free();
+
+};
+
+NS_END
